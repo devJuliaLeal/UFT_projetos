e2300f5dbcec41d62175c9b1ee819a10210384df
Julia Leal <120651477+devJuliaLeal@users.noreply.github.com>
2023-04-19 11:21:58 -0300

    commit

--- auladias19/main.c
+++ auladias19/main.c
@@ -14,18 +14,10 @@ Data: 19/04/2023
 int main()
 {
 
-
-       int i = 10;
-       while(i)
-       {
-           printf("9 x %2d = %2\n", i, 9*i);
-           i++;
-
-       }
-
-
-
-
+     while (1)
+     {
+         printf("PALMEIRAS NAO TEM MUNDIAL");
+     }
 
 
 }
