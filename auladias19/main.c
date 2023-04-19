dceb3ec14934f08f5fa448d7543a0934bd6a5693
Julia Leal <120651477+devJuliaLeal@users.noreply.github.com>
2023-04-19 11:17:52 -0300

    commit

--- auladias19/main.c
+++ auladias19/main.c
@@ -14,43 +14,14 @@ Data: 19/04/2023
 int main()
 {
 
-    unsigned a, b, c, d;
 
-    printf("a=");
-    scanf("%d", &a);
+       int i = 10;
+       while(i)
+       {
+           printf("9 x %2d = %2\n", i, 9*i);
+           i++;
 
-
-    printf("b=");
-    scanf("%d", &b);
-
-    printf("c=");
-    scanf("%d", &c);
-
-    printf("d=");
-    scanf("%d", &d);
-
-    printf("valores originais: \n");
-
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-    printf("c= %d\n", c);
-    printf("d = %d\n", d);
-
-    a++;
-    b--;
-    c = a;
-    d = c + b;
-    int soma = a+b+c+d;
-
-
-    printf("valores atuais: \n");
-
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-    printf("c= %d\n", c);
-    printf("d = %d\n", d);
-
-
+       }
 
 
 
