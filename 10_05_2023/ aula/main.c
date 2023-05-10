40f3b06e80073bc1f280417201ce6a80b8f41311
Julia Leal <120651477+devJuliaLeal@users.noreply.github.com>
2023-05-10 09:32:48 -0300

    commit

--- 10_05_2023/ aula/main.c
+++ 10_05_2023/ aula/main.c
@@ -1,47 +1,124 @@
+/*--- BIBLIOTECAS ---*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+
+/*--- BIBLIOTECAS ---*/
+
+
+/*--- PROTÓTIPO DAS FUNÇÕES ---*/
+
+void asteriscos();
+void sustenido();
+void dolar();
+void generica(char chr);
+void generica2 (int num2);
+
+
+
+/*--- PROTÓTIPO DAS FUNÇÕES ---*/
+
+
+/*---  FUNÇÃO PRINCIPAL ---*/
+
+
+
+/*---  FUNÇÃO PRINCIPAL ---*/
+
+
 
 int main()
 {
 
 setlocale(LC_ALL, "Portuguese_Brazil");
 
-    int num1=0, num2=0;
+unsigned numero;
 
-    num1 = num1 + 1;
-
-    printf("num1 = %2d\n", num1);
-
-    num2 += 1;
-
-    printf("num2 = %2d\n", num2);
-
-    if(num1 == num2)
-        printf("As atibuições são iguais\n");
-        else
-            printf("Não são iguais");
+    while(1){
+        printf("Digite um número: ");
+        scanf("%d", &numero);
+        if(numero == 1) asteriscos();
+        if(numero == 2) sustenido();
+        if(numero == 3) dolar();
+        if(numero == 4) arroba();
+        if(numero == 5) generica('*');
+        if(numero == 6) generica2(2);
 
 
-
-int num3=0, num4=0;
-
-    num3 = num3 - 1;
-
-    printf("num3 = %2d\n", num3);
-
-    num4 -= 1;
-
-    printf("num4 = %2d\n", num4);
-
-    if(num3 == num4)
-        printf("As atibuições são iguais");
-        else
-            printf("Não são iguais");
+        if(!numero) break;
+    }
 
 
 
 
 
+
+
+   return 0;
 
 }
+
+/*--- DESENVOLVIMENTO DAS FUNÇÕES ---*/
+
+void asteriscos()
+
+{
+    char asts;
+    for(asts=0; asts<10; asts++)
+        putchar('*');
+    putchar('\n');
+}
+
+
+void sustenido()
+
+{
+    char asts;
+    for(asts=0; asts<10; asts++)
+        putchar('#');
+    putchar('\n');
+}
+
+void dolar()
+
+{
+    char asts;
+    for(asts=0; asts<10; asts++)
+        putchar('$');
+    putchar('\n');
+}
+void arroba()
+
+{
+    char asts;
+    for(asts=0; asts<10; asts++)
+        putchar('@');
+    putchar('\n');
+}
+
+
+void generica(char chr)
+{
+    int asts;
+    for(asts=0; asts<10; asts++)
+        putchar(chr);
+    putchar('\n');
+
+}
+
+void generica2(int num2)
+{
+    int asts;
+    for(asts=0; asts<num2; asts++)
+        putchar('&');
+    putchar('\n');
+
+}
+
+
+
+
+
+
+/*--- DESENVOLVIMENTO DAS FUNÇÕES ---*/
