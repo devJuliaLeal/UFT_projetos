c4515df51a4e62a2c2b04b419bdabc345e303041
Julia Leal <120651477+devJuliaLeal@users.noreply.github.com>
2023-05-10 08:18:46 -0300

    commit

---
+++ 10_05_2023/ aula/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float a1 = 55.3;
+
+
+    a1 /=1.8;
+    printf("a1 = %.2f\n", a1);
+
+    int a2 =1;
+
+
+    a2 +=1;
+    printf("a2 = %d\n", a2);
+
+
+
+}
